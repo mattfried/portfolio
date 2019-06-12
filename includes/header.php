4c1314f2fb53a1ac6e291dc4b1d6b0a562f553ec
Matt Fried <mattfrieder@gmail.com>
2019-06-11 20:27:05 -0500

    updated header.php meta descriptions

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,15 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width", initial-scale="1", shrink-to-fit="no">
+    <meta name="description"
+    content="<?php if ($current == 'about') {
+        echo "Hi! I'm Matt Fried. I'm a web developer currently specializing in front-end development. This is my portfolio showcasing my journey as a web developer.";
+      }    else if ($current == 'projects') {
+        echo "Web development gives me the power to build and share detailed, elegant apps with the world around me. Here you will find the projects I've created.";
+      } else if ($current == 'contact') {
+        echo "Looking to hire a front-end web developer? Please don't hesitate to contact me.";
+      }
+      ?>">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="../css/normalize.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:400,500,600,700,800|Questrial|family=Raleway:100,200,300,400,500" />
